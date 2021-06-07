acd1b5a6f52b214c8b9b7d4677e92e570c5e983d
jkuehner <jochen.kuehner@kardex.com>
2021-06-07 12:52:03 +0200

    work on puting it into a library

---
+++ src/globals.ts
@@ -0,0 +1,11 @@
+import type { Socket } from 'socket.io'
+
+export { };
+
+declare global {
+  interface Window {
+    io: { connect: (name: string, par: any) => Socket };
+    socketUrl: string;
+    registerSocketOnLoad: Function;
+  }
+}
