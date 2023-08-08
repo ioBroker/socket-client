d98c383de7d5c829a7cabd598701e08dfc285478
GermanBluefox <dogafox@gmail.com>
2023-08-08 21:33:25 +0200

    Corrected potential error

--- src/tools.ts
+++ src/tools.ts
@@ -13,14 +13,14 @@ export function getObjectViewResultToArray<T extends ioBroker.Object>(
 
 /** Makes sure that a host id starts with "system.host." */
 export function normalizeHostId(host: string): string {
-	if (!host.startsWith("system.host.")) {
+	if (!host?.startsWith("system.host.")) {
 		host = `system.host.${host}`;
 	}
 	return host;
 }
 
 export function objectIdToHostname(id: string): string {
-	if (id.startsWith("system.host.")) {
+	if (id?.startsWith("system.host.")) {
 		id = id.substr("system.host.".length);
 	}
 	return id;
