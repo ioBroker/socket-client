1045d664a46d71faf57358f714929c89580da834
GermanBluefox <dogafox@gmail.com>
2024-06-06 22:19:13 +0700

    Comment

--- src/Connection.ts
+++ src/Connection.ts
@@ -260,7 +260,9 @@ export class Connection<
 
 		let host = this.props.host;
 		let port = this.props.port;
-		let protocol = (this.props.protocol || window.location.protocol).replace(":", "");
+		let protocol = (
+			this.props.protocol || window.location.protocol
+		).replace(":", "");
 		let path = window.location.pathname;
 
 		if (
